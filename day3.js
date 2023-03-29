a1496d2b83f0c118ac7617b68310a6062cabfd27
mugisha <mugishaelvis456@gmail.com>
2023-03-29 14:13:49 +0200

    getting datas from a form

---
+++ day3.js
@@ -0,0 +1,21 @@
+const savu=document.getElementById("save");
+savu.addEventListener("click",()=>{
+    console.log("hello")
+})
+
+savu.addEventListener("dblclick",()=>{
+    alert("hello double")
+})
+const form=document.getElementById("myform");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();// add event in () and and this line of event.preventDefault()
+    let text=form.username.value;
+    let password=form.pswd.value;
+     let total=text +' ' + 'and' + ' ' + password
+     alert(total)
+    form.reset();// then to clear your code 
+})
+
+
+
